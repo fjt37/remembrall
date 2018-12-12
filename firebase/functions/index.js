95b968bc44489a0c54029871ce958a92e24eced2
fjt37 <fjt37@cornell.edu>
2018-12-12 02:09:46 -0500

    done!

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -2,21 +2,30 @@
 
 const functions = require('firebase-functions');
 const {dialogflow, Confirmation} = require('actions-on-google');
+const natural = require('natural');
 
-const app = dialogflow({
-    debug: true,
-    clientId : '336474509170-2d0041msj7vk6m0nbcpf3auhot3qavrm.apps.googleusercontent.com'
-});
+//// Helpers ////
+
+const tokenizer = new natural.WordTokenizer() // removes all punctuation
+
+const replacements = [
+    ['I am', 'you are'], 
+    ['I was', 'you were'], 
+    ['I', 'you'], 
+    ['me', 'you'], 
+    ['my', 'your'], 
+    ['mine', 'yours']
+];
+
 
 function second_person(s) {
-    return s
-        .replace(/( |^)I am([ .,:;/-]|$)/i,  "you are")
-        .replace(/( |^)I was([ .,:;/-]|$)/i, "you were")
-        .replace(/( |^)I([ .,:;/-]|$)/i,     "you")
-        .replace(/( |^)me([ .,:;/-]|$)/i,    "you")
-        .replace(/( |^)my([ .,:;/-]|$)/i,    "your")
-        .replace(/( |^)mine([ .,:;/-]|$)/i,  "yours")
-        ;
+    replacements.forEach(function(pair) {
+        var pattern = RegExp('( |^)(' + pair[0] + ')([ .,\':;/-]|$)', 'i');
+        s = s.replace(pattern, function(match, g1, g2, g3) {
+            return match.replace(g2, pair[1]);
+        });
+    });
+    return s;
 }
 
 function strip_deep_link(input) {
@@ -31,10 +40,12 @@ function strip_deep_link(input) {
 }
 
 function tokenize(s) {
-    var tokens = s.split(/\s+/);
+    var tokens = tokenizer.tokenize(s);
     console.log(JSON.stringify(tokens));
     return tokens;
 }
+
+//// Primary Logic ////
 
 function remember(conv, s) {
     console.log("REMEMBERING");
@@ -76,18 +87,35 @@ function recall(conv, s) {
         if (e > evidence[max_evidence_id]) {
             max_evidence_id = id;
         }
+        else if (e == evidence[max_evidence_id] && conv.user.storage.mem.statements[id].length < conv.user.storage.mem.statements[max_evidence_id].length) {
+            max_evidence_id = id;
+        }
     });
+    if (evidence[max_evidence_id] < 1) {
+        max_evidence_id = -1;
+    }
     return max_evidence_id;
 }
+
+//// Dialogflow ////
+
+const app = dialogflow({
+    debug: true,
+    clientId : '336474509170-2d0041msj7vk6m0nbcpf3auhot3qavrm.apps.googleusercontent.com'
+});
 
 app.intent('remember', (conv) => {
     var to_remember = strip_deep_link(conv.input.raw);
     if (/remember[, ]*(?:that +)?/i.test(to_remember)) {
-        var split = to_remember.split(/remember[, ]*(?:that +)?/i);;
+        var split = to_remember.split(/remember[, ]*(?:that *)?/i);;
         to_remember = split.slice(1).join(" remember ");
     }
-    remember(conv, to_remember);
-    conv.ask("Alright, I'll remember that " + second_person(to_remember))
+    if (to_remember.length > 0) {
+        remember(conv, to_remember);
+        conv.ask("Alright, I'll remember that " + second_person(to_remember));
+    } else {
+        conv.ask("What would you like me to remember?");
+    }
 });
 
 app.intent('recall', (conv) => {
@@ -95,11 +123,15 @@ app.intent('recall', (conv) => {
     if (conv.user.storage.mem) {
         var statement_id = recall(conv, query)
         console.log(statement_id);
-        var s = conv.user.storage.mem.statements[statement_id];
-        console.log(s);
-        s = second_person(s)
-        s = s.charAt(0).toUpperCase() + s.substring(1)
-        conv.ask(s);
+        if (statement_id >= 0) {
+            var s = conv.user.storage.mem.statements[statement_id];
+            console.log(s);
+            s = second_person(s)
+            s = s.charAt(0).toUpperCase() + s.substring(1)
+            conv.ask(s);
+        } else {
+            conv.ask("I don't seem to have any relevant memories.")
+        }
     }
     else {
         conv.ask("I'm sorry, nothing has been remembered.");
@@ -111,18 +143,5 @@ app.intent('clear all memories', (conv) => {
     conv.user.storage = {};
     conv.ask("Okay, all memories have been forgotten.")
 })
-
-// app.intent('forget', (conv) => {
-//     conv.ask(new Confirmation("Are you sure you want to forget everything?"))
-// });
-
-// app.intent('confirm forget', (conv, params, confirmed) => {
-//     if (confirmed) {
-//         conv.user.storage = {};
-//         conv.ask("Alright, I've forgotten everything.");
-//     } else {
-//         conv.ask("Okay, don't worry, I'll keep remembering.");
-//     }
-// });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
